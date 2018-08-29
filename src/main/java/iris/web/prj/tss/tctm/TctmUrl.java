a10eda25cfb7e2e221f78162aedca8e2f62fb08e
newiris1 <newiris1@cicd_mig.com>
2018-08-29 00:01:07 +0000

    기술팀 과제

---
+++ src/main/java/iris/web/prj/tss/tctm/TctmUrl.java
@@ -0,0 +1,24 @@
+package iris.web.prj.tss.tctm;
+
+public class TctmUrl {
+
+    public static final String jspHome = "web/prj/tss/tctm";
+    public static final String doHome = "/prj/tss/tctm";
+
+    public static final String jspList = jspHome + "/tctmTssList";
+    public static final String jspWrite = jspHome + "/";
+    public static final String jspView = jspHome + "/tctmTssPlnDetail";
+    public static final String jspTabSum = jspHome + "/pln/tctmTssPlnSmryIfm";
+    public static final String jspTabGoal = jspHome + "/pln/tctmTssPlnGoalYldIfm";
+
+    public static final String doList = doHome + "/tctmTssList.do";
+    public static final String doView = doHome + "/tctmTssPlnDetail.do";
+    public static final String doTabSum = doHome + "/tctmTssPlnSmryIfm.do";
+    public static final String doTabGoal = doHome + "/tctmTssPlnGoalYldIfm.do";
+
+    public static final String doSelectList = doHome + "/tctmTssSelectList.do";
+    public static final String doUpdateInfo = doHome + "/tctmTssUpdateInfo.do";
+    public static final String doDeleteInfo = doHome + "/tctmTssDeleteInfo.do";
+
+
+}
