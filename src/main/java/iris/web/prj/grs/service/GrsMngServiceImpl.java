31ff6d7cfcded52355bb23f800719973776393d5
IRIS05 <IRIS05@cicd_mig.com>
2019-01-31 05:35:45 +0000

    P1일 경우 리더에게 메일 발송금지

--- src/main/java/iris/web/prj/grs/service/GrsMngServiceImpl.java
+++ src/main/java/iris/web/prj/grs/service/GrsMngServiceImpl.java
@@ -1,5 +1,20 @@
 package iris.web.prj.grs.service;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.velocity.VelocityEngineUtils;
+
 import devonframe.configuration.ConfigService;
 import devonframe.dataaccess.CommonDao;
 import devonframe.util.NullUtil;
@@ -8,14 +23,6 @@ import iris.web.common.util.StringUtil;
 import iris.web.prj.tss.gen.service.GenTssPlnService;
 import iris.web.prj.tss.gen.service.GenTssService;
 import iris.web.tssbatch.service.TssStCopyService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.velocity.app.VelocityEngine;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.velocity.VelocityEngineUtils;
-
-import javax.annotation.Resource;
-import java.util.*;
 
 @Service("grsMngService")
 public class GrsMngServiceImpl implements GrsMngService {
@@ -80,8 +87,6 @@ public class GrsMngServiceImpl implements GrsMngService {
 			int seqMax = Integer.parseInt(String.valueOf(getWbs.get("seqMax")));
 			String seqMaxS = String.valueOf(seqMax + 1);
 
-
-//				input.put("nprodSalsPlnY", (Integer)input.get("nprodSalsPlnY") * 100000000);
 			input.put("wbsCd", input.get("tssScnCd") + seqMaxS);
 			input.put("pkWbsCd", input.get("wbsCd"));
 			input.put("userId", input.get("_userId"));
@@ -91,43 +96,31 @@ public class GrsMngServiceImpl implements GrsMngService {
 			if (grsYn.equals("Y")) {
 				input.put("pgsStepCd", "PL");                                // 과제 진행 단계 코드
 				input.put("tssSt","101");
+				//input.put("grsStCd","101");
 			}else if (grsYn.equals("N")) {
 				input.put("pgsStepCd","PL");
 				input.put("tssSt","100");
 			}
 
-			LOGGER.debug("=============== 과제 기본정보 등록 ===============");
-			// mchnCgdgService.saveCgdsMst(input);
+			//LOGGER.debug("=============== 과제 기본정보 등록 ===============");
 			updateGrsInfo(input);                                                        //과제 기본정보 등록
 			input.put("tssCd", input.get("newTssCd"));
 			result.put("tssCd", (String) input.get("newTssCd"));
-			LOGGER.debug("=============== 생성TSS_CD : "+input.get("newTssCd")+" ===============");
-
+			//LOGGER.debug("=============== 생성TSS_CD : "+input.get("newTssCd")+" ===============");
 
 			if (grsYn.equals("Y")) {
-				LOGGER.debug("=============== GRS=Y 인경우 GRS 요청정보 생성 ===============");
+				//LOGGER.debug("=============== GRS=Y 인경우 GRS 요청정보 생성 ===============");
 				input.put("grsEvSt", "P1");
 				input.put("dlbrCrgr", input.get("_userSabun"));
 				updateGrsReqInfo(input);                                            //GRS 정보 등록
 			}else if (grsYn.equals("N")) {
-				LOGGER.debug("=============== GRS=N 인경우 마스터 이관 ===============");
+				//LOGGER.debug("=============== GRS=N 인경우 마스터 이관 ===============");
 				String tssCd = (String) input.get("tssCd");
-
-				LOGGER.debug("=============== 과제정보 마스터 이관(PL) ===============");
+				//LOGGER.debug("=============== 과제정보 마스터 이관(PL) ===============");
 				input.put("fromTssCd", tssCd); //GRS 기본정보 TSS_CD
 				moveDefGrsDefInfo(input);
 
-
-					/*
-					LOGGER.debug("=============== 과제정보 마스터 이관(PG) ===============");
-					input.put("tssCd", tssCd.substring( 0,9) + (java.lang.Integer.parseInt(tssCd.substring( 9,10))+1));
-					input.put("pgsStepCd","PG");
-					input.put("tssSt","100");
-					moveDefGrsDefInfo(input);
-					*/
-
-
-				LOGGER.debug("=============== GRG 과제 기본정보 삭제 ===============");
+				//LOGGER.debug("=============== GRG 과제 기본정보 삭제 ===============");
 				input.put("tssCd", tssCd);
 				deleteDefGrsDefInfo(input);
 
@@ -168,38 +161,36 @@ public class GrsMngServiceImpl implements GrsMngService {
 				input.put("grsEvSt","M");
 			}
 		}
-
-		LOGGER.debug("===GRS 평가 완료 등록===");
+		
+		//LOGGER.debug("===GRS 평가 완료 등록===");
 		grsReqService.insertGrsEvRsltSave(dsLst, input);
-
-
-		LOGGER.debug("===GRS 평가 완료후 과제 상태값 변경 102===");
-
-
+				
+		//LOGGER.debug("===GRS 평가 완료후 과제 상태값 변경 102===");
 		if(input.get("egrsEvSt").toString().indexOf("M")>-1 && input.get("grsEvMType").equals("IN")){
 			// 중간 평가 > 진척도 점검 IN 인경우 >> 진행/진행중
 			input.put("tssSt", "100");
 		}else{
 			input.put("tssSt", "102");
+			//input.put("grsStCd", "102");
 			// 그외 평가완료
 		}
-
-
-
+		
 		genTssPlnService.updateGenTssPlnMstTssSt(input);		// tssSt update
 		grsMngService.updateDefTssSt(input);							// tssSt update
 
 			if (grsMngService.isBeforGrs(input).equals("1")) {
-				LOGGER.debug("===GRS 관리에서 기본정보를 입력한경우===");
-				LOGGER.debug("===과제 관리 마스터로 데이터 복제 (과제정보, 개요)===");
+				//LOGGER.debug("===GRS 관리에서 기본정보를 입력한경우===");
+				//LOGGER.debug("===과제 관리 마스터로 데이터 복제 (과제정보, 개요)===");
 				input.put("fromTssCd", input.get("tssCd"));
 				input.put("pgsStepCd", "PL");
 				grsMngService.moveDefGrsDefInfo(input);
 				grsMngService.deleteDefGrsDefInfo(input);
 			}
 
-		LOGGER.debug("===해당과제 리더에게 완료 메일 발송===");
-		genTssPlnService.retrieveSendMail(input); //개발에서 데이터 등록위해 반영위해 주석 1121
+		//LOGGER.debug("===해당과제 리더에게 완료 메일 발송===  :  " + input.get("egrsEvSt").equals("P1"));
+		if(!input.get("egrsEvSt").equals("P1") ){
+			genTssPlnService.retrieveSendMail(input); //개발에서 데이터 등록위해 반영위해 주석 1121
+		}
 
 		result.put("rtnMsg", "평가완료되었습니다.");
 		result.put("rtnSt", "S");
@@ -226,7 +217,7 @@ public class GrsMngServiceImpl implements GrsMngService {
 
 	@Override
 	public void moveDefGrsDefInfo(HashMap<String, Object> input) {
-		LOGGER.debug("=============== GRS기본정보를 과제 마스터로 등록(마스터, 개요, 산출물) ===============");
+		//LOGGER.debug("=============== GRS기본정보를 과제 마스터로 등록(마스터, 개요, 산출물) ===============");
 		String tssScnCd = (String) input.get("tssScnCd");
 		String tssAttrCd = (String) input.get("tssAttrCd");
 		String dropYn = (String) input.get("dropYn");
@@ -236,14 +227,19 @@ public class GrsMngServiceImpl implements GrsMngService {
 			// 국책인경우 1년차수 입력
 			input.put("tssNosSt", "1");
 		}
+		
+		if(dropYn!=null && dropYn.equals("Y")){
+			input.put("delYn", "Y");	
+		}else{
+			input.put("delYn", "N");	
+		}
 
+		LOGGER.debug("===============moveGrsDefInfo =============== : " + input);
 		//GRS 기본정보 과제 관리 마스터로 복제
 		commonDao.insert("prj.grs.moveGrsDefInfo", input);
-
+		
 		//================== Drop 과제인 경우 기본 정보 마스터만 이관 후 return ======================//
-		if(dropYn!=null && dropYn.equals("Y"))return;
-
-
+		if(dropYn!=null && dropYn.equals("Y")) return;
 
 		if(tssScnCd.equals("D")){
 			//		창호 01/장식재 03 > 건장재01
@@ -277,29 +273,18 @@ public class GrsMngServiceImpl implements GrsMngService {
 			commonDao.update("prj.grs.updateGrsDefInfo02", input);
 		}
 
-/*
-
-		if("N".equals(input.get("grsYn"))){
-			//GRS(P1)을 하지 않는 경우
-			//WBS 코드 생성 및 등록
-			String wbsCd = tssStCopyService.createWbsCd(input);
-			input.put("wbsCd", wbsCd);
-			commonDao.insert("prj.tss.com.updateTssMstWbsCd", input);
-		}
-*/
-
-		LOGGER.debug("=============== GRS 기본정보 과제 서머리 마스터로 복제 ===============");
+		//LOGGER.debug("=============== GRS 기본정보 과제 서머리 마스터로 복제 ===============");
 		commonDao.insert("prj.grs.moveGrsDefSmry", input);
 
 		Calendar cal = Calendar.getInstance();
         int mm   = cal.get(Calendar.MONTH) + 1;
 
-		LOGGER.debug("=============== 기본 산출물 등록 ===============");
-
+		//LOGGER.debug("=============== 기본 산출물 등록 ===============");
 		input.put("attcFilId", "");	 // 최초 등록시 파일 초기화
 		String goalYStart = input.get("tssStrtDd").toString().substring(0, 4);
 		String goalYEnd = input.get("tssFnhDd").toString().substring(0, 4);
 		String arslYymm01 = input.get("tssFnhDd").toString().substring(0, 7);
+		
 		if(tssScnCd.equals("G")){
 			//일반
 			//과제 제안서
