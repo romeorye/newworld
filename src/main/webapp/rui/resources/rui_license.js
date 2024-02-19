2c44bd518ac915dd843a711d6a8552c0f83ce849
IRIS05 <IRIS05@cicd_mig.com>
2024-02-19 05:36:48 +0000

    인수인계 소스 일원화

---
+++ src/main/webapp/rui/resources/rui_license.js
@@ -0,0 +1,39 @@
+/********************************************!!!!매우중요!!!!*********************************************
+ ** 1. 사내 사용시 KAMS를 통해 요청하여 사용허가를 받아야만 소프트웨어 라이센스 계약서에 동의하는 것으로 간주됩니다.
+ ** 2. DevOn RUI가 포함된 제품을 판매할 경우에도 KAMS를 통해 요청하여 사용허가를 받아야만 합니다.
+ ** 3. KAMS를 통해 사용허가를 받지 않은 경우 소프트웨어 라이센스 계약을 위반한 것으로 간주됩니다.
+ ** 4. 별도로 판매될 경우 LGCNS의 소프트웨어 판매정책을 따릅니다. (KAMS에 문의 바랍니다.)
+ ** 5. 원저자의 허락없이 재배포 할 수 없으며 LG CNS 외부로의 유출을 하여서는 안 된다.
+ ** 7J2064+Z6re8
+ ** 7J2064+Z6re8KGRnbGVlQGxnY25zLmNvbSk=
+ ** TEftlZjsmrDsi5zsiqQg7ISc67mE7IqkIOyEpOuqheyEnCAoMjAyMeuFhClbUFMxMjIxMjgzOUJd
+ ** 12e0ffa5-cf7e-4ac9-852b-da5d4649c80b
+ ***********************************************************************************************************/
+
+if(typeof Rui != 'undefined') {
+    Rui.License = 'bGdoYXVzeXMuY29tO2x4aGF1c3lzLmNvbQ==';
+}
+
+if(typeof DU != 'undefined')
+    DU.License = 'bGdoYXVzeXMuY29tO2x4aGF1c3lzLmNvbQ==';
+
+if(typeof Rui.PK == 'undefined')  {
+  Rui.PK = {
+      "ex" : "10001",
+      "n" : "9a2f202b5a6c97072a70c1b391ba440002e109942361a726918c053d830805e9d2793610162bfba131e838be723f3e72eec57297c4596ee0da7c29f90699728d",
+      "ss" : "5bfec243e26543f88c41a62086808290b598090523102431446121ccfd76b1e3d5c508978d3d096756feaef343907555358600fa60dc0c09ff0d3ea3a6a5baf7",
+      "u" : {
+    	  "v"  : "dHJ1ZQ==",
+    	  "ss" : "846554c2c056cbeddc18ba2feefb55467ed0c259ec20d678762616cf0c2e4c22d545a71e3185813d7859f53894cad494a6a3a59dd88047de618f236b411eaeac" 
+      },
+      "s" : 
+      	{ "v"  : "MjAyMS0wNi0zMA==",
+      		"ss" : "95286cc378e46352d992223741382facc98b9094d9e44f31d4fee71396321178e8cbd8fec6db6b103ce452a47da399e8488cda031594288d6ff46e0ed7cc36ed" },
+      "e" : 
+      	{ "v" : "MjExMS0wNi0zMA==",
+      		"ss" : "5a41b612f7ab17f2b3f6134ec63028af77575b62e8820416bfe7c9f0d540a5da096859b94a6e86e8631716766d0e44c4fa9b23fb289f00992e4886996bca8057" },
+      "f" : 
+      	{  }
+      		
+  }
+}
