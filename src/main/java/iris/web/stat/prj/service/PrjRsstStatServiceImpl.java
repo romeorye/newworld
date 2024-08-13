db7939ddacda780a918095bc4ec46cd6aee2c68e
서성일 <siseo@lxhausys.com>
2024-08-13 12:22:13 +0900

    [20240813]지식재산 db 오류 - 예외처리 적용

--- src/main/java/iris/web/stat/prj/service/PrjRsstStatServiceImpl.java
+++ src/main/java/iris/web/stat/prj/service/PrjRsstStatServiceImpl.java
@@ -39,19 +39,22 @@ public class PrjRsstStatServiceImpl implements PrjRsstStatService {
 		prjList = commonDao.selectList("stat.prj.retrievePrjRsstStatList", input);
 	
 		//실적 지적재산권 조회 후 삽입처리
-		for(int i=0;i < prjList.size(); i++) {
-			Map<String, Object> tmpPrjtMap = prjList.get(i);
-			
-			searchParam = new HashMap<String, Object>();
-			searchParam.put("searchYear" , input.get("yyyy"));
-			searchParam.put("wbsCd"      , tmpPrjtMap.get("wbsCd"));
-			prptList = commonDaoPims.selectList("prj.rsst.ptotprpt.oracle.retrievePrjErpPrptByYearList", searchParam);
-			
-			if( prptList != null && prptList.size() > 0 ) {
-				prjList.get(i).put("prptArslCnt", prptList.get(0).get("patCnt"));  
-			}
+		try {
+    		for(int i=0;i < prjList.size(); i++) {
+    			Map<String, Object> tmpPrjtMap = prjList.get(i);
+    			
+    			searchParam = new HashMap<String, Object>();
+    			searchParam.put("searchYear" , input.get("yyyy"));
+    			searchParam.put("wbsCd"      , tmpPrjtMap.get("wbsCd"));
+    			prptList = commonDaoPims.selectList("prj.rsst.ptotprpt.oracle.retrievePrjErpPrptByYearList", searchParam);
+    			
+    			if( prptList != null && prptList.size() > 0 ) {
+    				prjList.get(i).put("prptArslCnt", prptList.get(0).get("patCnt"));  
+    			}
+    		}
+		} catch (Exception e) {
+		    e.printStackTrace();
 		}
-		
 		return prjList;
 	}
 	
